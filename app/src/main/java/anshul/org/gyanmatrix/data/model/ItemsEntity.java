6b54a4ccab6b9ea0a71d53e1dc7972499b405852
Anshul <iamanshulkabra@gmail.com>
2017-08-09 22:36:04 +0530

    Change the model class serialization  to parcle .

--- app/src/main/java/anshul/org/gyanmatrix/data/model/ItemsEntity.java
+++ app/src/main/java/anshul/org/gyanmatrix/data/model/ItemsEntity.java
@@ -1,10 +1,13 @@
 package anshul.org.gyanmatrix.data.model;
 
-import java.io.Serializable;
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import java.util.ArrayList;
 import java.util.List;
 
 
-public class ItemsEntity implements Serializable{
+public class ItemsEntity implements Parcelable {
 
     private String quote_max;
     private String quote_available;
@@ -34,5 +37,44 @@ public class ItemsEntity implements Serializable{
         this.records = records;
     }
 
+    protected ItemsEntity(Parcel in) {
+        quote_max = in.readString();
+        quote_available = in.readString();
+        if (in.readByte() == 0x01) {
+            records = new ArrayList<RecordsEntity>();
+            in.readList(records, RecordsEntity.class.getClassLoader());
+        } else {
+            records = null;
+        }
+    }
 
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(quote_max);
+        dest.writeString(quote_available);
+        if (records == null) {
+            dest.writeByte((byte) (0x00));
+        } else {
+            dest.writeByte((byte) (0x01));
+            dest.writeList(records);
+        }
+    }
+
+    @SuppressWarnings("unused")
+    public static final Parcelable.Creator<ItemsEntity> CREATOR = new Parcelable.Creator<ItemsEntity>() {
+        @Override
+        public ItemsEntity createFromParcel(Parcel in) {
+            return new ItemsEntity(in);
+        }
+
+        @Override
+        public ItemsEntity[] newArray(int size) {
+            return new ItemsEntity[size];
+        }
+    };
 }
--- app/src/main/java/anshul/org/gyanmatrix/data/model/RecordsEntity.java
+++ app/src/main/java/anshul/org/gyanmatrix/data/model/RecordsEntity.java
@@ -1,10 +1,9 @@
 package anshul.org.gyanmatrix.data.model;
 
-import java.io.Serializable;
+import android.os.Parcel;
+import android.os.Parcelable;
 
-
-public class RecordsEntity implements Serializable{
-
+public class RecordsEntity implements Parcelable {
 
     private String id;
     private String name;
@@ -14,6 +13,9 @@ public class RecordsEntity implements Serializable{
     private String matches_played;
     private String country;
 
+    public RecordsEntity(){
+
+    }
     public String getId() {
         return id;
     }
@@ -69,4 +71,43 @@ public class RecordsEntity implements Serializable{
     public void setCountry(String country) {
         this.country = country;
     }
+
+    protected RecordsEntity(Parcel in) {
+        id = in.readString();
+        name = in.readString();
+        image = in.readString();
+        total_score = in.readString();
+        description = in.readString();
+        matches_played = in.readString();
+        country = in.readString();
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(id);
+        dest.writeString(name);
+        dest.writeString(image);
+        dest.writeString(total_score);
+        dest.writeString(description);
+        dest.writeString(matches_played);
+        dest.writeString(country);
+    }
+
+    @SuppressWarnings("unused")
+    public static final Parcelable.Creator<RecordsEntity> CREATOR = new Parcelable.Creator<RecordsEntity>() {
+        @Override
+        public RecordsEntity createFromParcel(Parcel in) {
+            return new RecordsEntity(in);
+        }
+
+        @Override
+        public RecordsEntity[] newArray(int size) {
+            return new RecordsEntity[size];
+        }
+    };
 }
